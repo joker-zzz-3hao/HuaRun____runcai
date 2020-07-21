e17f787ca9feee2bfa2b39a859b00e9496e020f0
tengwei6 <tengwei6@crc.com.hk>
2020-07-21 10:15:29 +0800

    修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', {
+      "code": 124,// vscode代码格式化一行最大字符就是124，所以这里要用124
+      "tabWidth": 2,
+      // "comments": 100,//enforces a maximum line length for comments; defaults to value of code
+      // "ignorePattern": true,// ignores lines matching a regular expression; can only match a single line and need to be double escaped when written in YAML or JSON
+      "ignoreComments": false,// ignores all trailing comments and comments on their own line
+      "ignoreTrailingComments": false,// ignores only trailing comments
+      "ignoreUrls": true,// ignores lines that contain a URL
+      "ignoreStrings": true,// ignores lines that contain a double-quoted or single-quoted string
+      "ignoreTemplateLiterals": true,// ignores lines that contain a template literal
+      "ignoreRegExpLiterals": true,// ignores lines that contain a RegExp literal
+    }],
   },
 };
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/styles/public.css';
 
-Vue.use(Element)
+Vue.use(Element);
 
 Vue.config.productionTip = false;
 
