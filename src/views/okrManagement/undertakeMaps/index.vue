e042b1a05bdf511a58c4b56d6c17d65578a95b01
Lin Xinquan 林心荃 <xinquan.lin@163.com>
2020-08-07 15:26:05 +0800

    承接树

--- src/views/okrManagement/undertakeMaps/index.vue
+++ src/views/okrManagement/undertakeMaps/index.vue
@@ -1,40 +1,47 @@
 <template>
-  <div>
-    <vue-svg-tree
-      :treeData="treeData"
-      svgId="svg"
-      ref="svgTree"
-      :curveness="false"
-      direction="col"
-      fatherId="okrfid"
-      childId="cid"
-    >
-      <div slot="treecard" slot-scope="props">
-        <div class="text">
-          <div class="node_title">
-            <span class="OKR">OKR</span>
-            <span class="label">OKR{{props.node.name}}</span>
-          </div>
-          <div class="node_des">
-            <div>{{props.node.content}}</div>
-            <div>{{props.node.open}}</div>
-            <div>{{$route.params.objectName}}</div>
-          </div>
-          <div class="showTips" :class="props.node.children ? 'havechild' : ''">
-            <a @click="open(props.node)" class="tips_icon icon_edit">
-              <i v-if="props.node.open" class="el-icon-minus"></i>
-              <i v-else class="el-icon-plus"></i>
-            </a>
-          </div>
+  <div class="home">
+    <div style="margin-left:20px;" v-if="showAll">
+      <department
+        :data="cycleData"
+        type="cycleListSelect"
+        @handleData="handleCycleData"
+        :defaultProps="cycleDefaultProps"
+      ></department>
+    </div>
+    <div style="margin-left:20px;" v-if="showAll">
+      <department
+        type="department"
+        :data="departmentData"
+        :initDepartment="initDepartment"
+        @handleData="handleData"
+        :defaultProps="departmentDefaultProps"
+      ></department>
+    </div>
+    <!-- 要展示多个 -->
+    <div v-for="item in svgList" :key="item.id">
+      <vue-svg-tree
+        :treeData="treeData"
+        :svgId="'svg'+item.id"
+        ref="svgTree"
+        :curveness="false"
+        direction="col"
+        fatherId="okrParentDetailId"
+        childId="okrDetailId"
+      >
+        <div slot="treecard" slot-scope="props">
+          <card :node="props.node"></card>
         </div>
-      </div>
-    </vue-svg-tree>
+      </vue-svg-tree>
+    </div>
   </div>
 </template>
 
 <script>
+import department from '@/components/department';
 import svgtree from '@/components/svgtree';
+import card from './components/card';
 import Server from './server';
+import CONST from './const';
 
 const server = new Server();
 
@@ -42,142 +49,125 @@ export default {
   name: 'supportMaps',
   components: {
     'vue-svg-tree': svgtree,
+    card,
+    department,
   },
   data() {
     return {
+      CONST,
       server,
       treeData: [],
+      searchForm: {
+        okrDetailId: '',
+        oorKr: 'o',
+        orgId: '888822223333',
+        periodId: 'periodId',
+      },
+      cycleData: [],
+      departmentData: [],
+      initDepartment: {},
+      cycleDefaultProps: {
+        children: 'children',
+        label: 'periodName',
+        id: 'periodId',
+      },
+      cycleObj: {
+        old: {
+          checkStatus: 0,
+          children: [],
+          periodName: '历史OKR周期',
+          okrCycleType: '0',
+          periodId: '0',
+        },
+        current: {
+          checkStatus: 1,
+          children: [],
+          periodName: '当前的OKR周期',
+          okrCycleType: '0',
+          periodId: '1',
+        },
+      },
+      departmentDefaultProps: {
+        children: 'children',
+        label: 'orgName',
+        id: 'orgId',
+      },
+      svgList: [{
+        id: 1, name: '赚1个亿',
+      }],
+      showAll: true,
     };
   },
   created() {
     this.init();
+    this.showAll = this.$route.params.showAll;
+    this.searchForm.okrDetailId = this.$route.params.okrDetailId;
   },
   methods: {
     init() {
-      // 查承接地图
-      this.server.getmaps().then((res) => {
-        if (res.code == 200) {
-          console.log('承接地图', res.data);
-          this.treeData = res.data;
+      const self = this;
+      if (this.showAll) {
+        // 查询周期
+        self.server.getOkrCycleList().then((res) => {
+          if (res.data.length > 0) {
+            res.data.forEach((item) => {
+            // checkStatus为0时是历史周期，1为当前周期
+              if (item.checkStatus == '0') {
+                self.cycleObj.old.children.push(item);
+              } else if (item.checkStatus == '1') {
+                self.cycleObj.current.children.push(item);
+              }
+            });
+            self.pushCycleObj('current');
+            self.pushCycleObj('old');
+          }
+          self.getmaps();
+        });
+        self.getOrgTable();
+      } else {
+        self.getmaps();
+        this.svgList = [{
+          id: 1, name: '赚1个亿',
+        }];
+      }
+      // 查询组织树
+    },
+    pushCycleObj(key) {
+      if (this.cycleObj[key].children.length > 0) {
+        this.cycleData.push(this.cycleObj[key]);
+      }
+    },
+    getOrgTable() {
+      // 查询组织树
+      this.server.getOrgTable().then((res) => {
+        if (res.code == '200') {
+          if (res.data) {
+            this.departmentData.push(res.data);
+          }
         }
       });
+    },
+    getmaps() {
+      // 查承接地图
+      this.server.getmaps(this.searchForm).then((res) => {
+        if (res.code == 200) {
+          this.treeData = res.data;
+          // this.treeData.forEach((item) => {
+          //   if (!item.okrParentDetailId) {
+          //     item.okrParentDetailId = '0';
+          //   }
+          // });
+          console.log('承接地图', this.treeData);
+        }
+      });
+    },
+    handleCycleData(data) {
+      this.okrCycle = data;
+      console.log(data);
+    },
+    handleData(data) {
+      console.log(data);
     },
   },
 };
 </script>
-<style>
-.vnode .text {
-  padding: 0 10px;
-}
-.vnode .text {
-  position: relative;
-  height: 100%;
-  max-width: 230px;
-  min-width: 216px;
-}
-.vnode .text .node_title {
-  padding: 3px 11px 0px 11px;
-  display: flex;
-  align-items: center;
-  flex-wrap: nowrap;
-}
-.vnode .text .node_title .OKR {
-  display: inline-block;
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  color: #fff;
-  line-height: 35px;
-  text-align: center;
-  font-size: 1.2rem;
-  background-color: #58c2ef;
-}
-.vnode .text .node_title .OKR.pink {
-  background-color: #e1244e;
-}
-.vnode .text .node_title .OKR.blue {
-  background-color: #58c2ef;
-}
-.vnode .text .node_title .label {
-  max-width: 130px;
-  height: 40px;
-  line-height: 40px;
-  overflow: hidden;
-  padding-left: 10px;
-  font-size: 1rem;
-  color: #1f1f1f;
-  display: inline-block;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-.vnode .text .node_title .label button {
-  overflow: hidden;
-  color: #1f1f1f !important;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  width: 140px;
-  display: inline-block;
-  line-height: 35px;
-  text-align: left;
-}
-.vnode .text .node_des {
-  display: flex;
-  flex-direction: column;
-  margin-left: 56px;
-  height: 60px;
-  color: #777777;
-  font-size: 12px;
-  justify-content: flex-start;
-}
-.vnode .text .node_des div {
-  width: 140px;
-  text-align: left;
-}
-.vnode .text .node_des div:nth-child(2) {
-  margin-top: 5px;
-}
-.vnode .text .node_progress {
-  position: absolute;
-  width: 200px;
-  bottom: 0px;
-  height: 40px;
-  border-top: 1px solid #d8d8d8;
-  line-height: 40px;
-}
-.vnode .text .node_progress .el-progress {
-  display: inline;
-}
-.vnode .text .node_progress .el-progress .el-progress__text {
-  font-size: 12px !important;
-  color: #777777 !important;
-}
-.vnode .text .showTips {
-  position: absolute;
-  left: 225px;
-  top: 0px;
-  opacity: 0;
-}
-.vnode .text .showTips a {
-  display: inline-block;
-}
-.vnode .text .showTips .tips_icon {
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  line-height: 20px;
-  text-align: center;
-  text-decoration: none;
-  padding: 3px;
-  background: #ffffff;
-  box-shadow: 1px 2px 10px 3px rgba(0, 0, 0, 0.08);
-  transition: all 0.5s ease 0s;
-  color: #777777;
-}
-.vnode .text .showTips .tips_icon:hover {
-  color: #58c2ef;
-}
-.vnode .text:hover .showTips.havechild {
-  color: #58c2ef;
-  opacity: 1;
-}
-</style>
--- src/views/okrManagement/undertakeMaps/server.js
+++ src/views/okrManagement/undertakeMaps/server.js
@@ -9,11 +9,34 @@ export default class Server extends ServerBase {
     return this._ajaxPost('gateway/crcloud-okrManage/getTimeCycle', param);
   }
 
+  // getmaps(param) {
+  //   return this._ajaxPost('gateway/crcloud-okrManage/getmaps', param);
+  // }
   getmaps(param) {
-    return this._ajaxPost('gateway/crcloud-okrManage/getmaps', param);
+    return this._ajaxPost('gateway/talent-query/okr/query/okrContinueMap', param);
   }
 
   getokrDetail(param) {
     return this._ajaxPost('gateway/talent-query/okr/query/okrDetail?okrId=111111222', param);
+  }
+
+  getOkrCycleList(param) {
+    return this._ajaxPost('gateway/talent-query/okr/query/period', param);
+  }
+
+  getOrgTable(param) {
+    return this._ajaxPost('gateway/talent-query/okr/query/orgTable', param);
+  }
+
+  getOkrTree(param) {
+    return this._ajaxPost(`gateway/talent-query/okr/query/okrTree?periodId=${param.periodId}`, param);
+  }
+
+  getDepartmentList(param) {
+    return this._ajaxPost('gateway/okr/departmentList', param);
+  }
+
+  searchKeyword(param) {
+    return this._ajaxPost('gateway/talent-query/okr/query/period', param);
   }
 }
