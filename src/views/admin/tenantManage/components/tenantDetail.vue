a97bf3c089da37df8bc10a6f18d6f188c139d669
xuzhipeng <357089360@qq.com>
2020-08-19 15:11:38 +0800

    修改菜单限制

--- src/views/admin/tenantManage/components/tenantDetail.vue
+++ src/views/admin/tenantManage/components/tenantDetail.vue
@@ -25,27 +25,20 @@
           <span>{{form.mobilePhone}}</span>
         </el-form-item>
         <el-form-item label="开放菜单功能">
-          <el-tree
-            disabled
-            @check-change="handleCheckChange"
-            ref="treeMenu"
-            :data="data"
-            show-checkbox
-            default-expand-all
-            node-key="id"
-          ></el-tree>
+          <div class="menuTreeList">
+            <div class="list" v-for="(item,index) in menuTreeList" :key="index">{{item}}</div>
+          </div>
         </el-form-item>
-        <!-- <el-form-item label="使用时间" prop="date">
-          <el-date-picker
-            v-model="form.date"
-            @change="changDate"
-            :disabled="infoBool"
-            type="datetimerange"
-            range-separator="至"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-          ></el-date-picker>
-        </el-form-item>-->
+        <div class="postMenu" v-show="false">
+          <el-cascader-panel
+            @change="handleCheckChange"
+            ref="treeMenu"
+            v-model="selectArr"
+            :options="data"
+            :props="{ multiple: true }"
+            node-key="id"
+          ></el-cascader-panel>
+        </div>
         <el-form-item label="租户状态">
           <span>{{CONST.STATUS[form.status]}}</span>
         </el-form-item>
@@ -92,36 +85,40 @@ export default {
       dialogTableVisible: false,
       dialogVisible: false,
       data: [],
+      menuTreeList: [],
+      selectArr: [],
     };
   },
   mounted() {
     this.dialogTableVisible = true;
     // 判断租户ID存在请求租户详情接口
-    if (this.tenantId) {
-      this.getTenant();
-      this.$nextTick(() => {
-        this.handleCheckChange();
-      });
-    }
+
     this.getqueryMenu();
   },
   methods: {
     // 获取选中tree key值 展示选中
     handleCheckChange() {
-      const keys = this.$refs.treeMenu.getCheckedKeys();
+      const keys = this.$refs.treeMenu.getCheckedNodes();
+      // eslint-disable-next-line array-callback-return
+      const keyCheck = keys.map((item) => {
+        if (item.children.length == 0) {
+          return item.data.label;
+        }
+      });
+      // eslint-disable-next-line array-callback-return
+      this.menuTreeList = keyCheck.filter((item) => {
+        if (item) {
+          return item;
+        }
+      });
       this.form.menuTree = keys.map((item) => ({ functionId: item }));
-    },
-    changDate(date) {
-      // eslint-disable-next-line prefer-destructuring
-      this.form.startTime = date[0];
-      // eslint-disable-next-line prefer-destructuring
-      this.form.endTime = date[1];
     },
     // 获取菜单功能树形结构
     getqueryMenu() {
       this.server.queryMenu()
         .then((res) => {
           this.data = this.getTreeList(res.data, true);
+          this.getTenant();
         });
     },
     // 递归修改符合element tree结构数据
@@ -130,7 +127,10 @@ export default {
         return data.map((item) => ({
           id: item.functionId,
           label: item.functionName,
+          value: item.functionCode,
+          checkStrictly: true,
           disabled: this.infoBool,
+          emitPath: false,
           children: this.getTreeList(item.children, disabled),
         }));
       }
@@ -139,18 +139,25 @@ export default {
     getTenant() {
       this.server.getTenant({ tenantId: this.tenantId })
         .then((res) => {
-          console.log(res);
           this.form.tenantName = res.data.tenantName;
           this.form.applyUser = res.data.applyUser;
           this.form.mobilePhone = res.data.mobilePhone;
           this.form.tenantID = res.data.tenantID;
           this.form.status = res.data.status;
-          // this.form.date = [new Date(res.data.startTime), new Date(res.data.endTime)];
-          // this.form.startTime = res.data.startTime;
-          // this.form.endTime = res.data.endTime;
-          const keys = res.data.menuTree.map((item) => item.functionId);
+          // eslint-disable-next-line array-callback-return
+          const keys = res.data.menuItems.map((item) => {
+            if (item) {
+              return item.split(':');
+            }
+          });
+          // eslint-disable-next-line array-callback-return
+          this.selectArr = keys.filter((item) => {
+            if (item) {
+              return item;
+            }
+          });
           this.$nextTick(() => {
-            this.$refs.treeMenu.setCheckedKeys(keys);
+            this.handleCheckChange();
           });
         });
     },
