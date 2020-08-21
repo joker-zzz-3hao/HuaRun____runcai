57ae49f00148f6cf50bb4a26e48008f29b621871
tengwei6 <tengwei6@crc.com.hk>
2020-08-21 09:27:12 +0800

    dialog更新

--- src/views/settings/okrCycle/components/addOkrCycle.vue
+++ src/views/settings/okrCycle/components/addOkrCycle.vue
@@ -57,10 +57,7 @@
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button type="primary" @click="submitForm" class="tl-btn amt-bg-slip">确定</el-button>
-      <el-button plain @click="close()" class="tl-btn amt-border-slip">
-        取消
-        <span class="lines"></span>
-      </el-button>
+      <el-button plain @click="close()" class="tl-btn amt-border-fadeout">取消</el-button>
     </div>
   </el-dialog>
 </template>
