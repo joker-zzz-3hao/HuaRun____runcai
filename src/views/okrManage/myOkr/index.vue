e6cbca47aed18c12f3c5279932a7d3e8a952806b
Lin Xinquan 林心荃 <xinquan.lin@163.com>
2020-08-27 10:48:34 +0800

    修改myokrindex

--- src/views/okrManage/myOkr/index.vue
+++ src/views/okrManage/myOkr/index.vue
@@ -5,16 +5,30 @@
       <el-tab-pane label="departmentOkrOKR" name="departmentOkr"></el-tab-pane>
     </el-tabs>-->
     <el-tabs v-model="activeName" @tab-click="goRoutesss">
-      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      <el-tab-pane label="我的OKR" name="myOkr"></el-tab-pane>
+      <el-tab-pane :label="departmentName+'OKR'" name="departmentOkr"></el-tab-pane>
     </el-tabs>
+    <el-button style="display:float" @click="goWriteOkr">创建okr</el-button>
+
     <router-view></router-view>
+    <el-drawer
+      :wrapperClosable="false"
+      :modal-append-to-body="false"
+      title="创建okr"
+      :visible.sync="createokrDrawer"
+      :before-close="handleClose"
+      class="tl-drawer"
+    >
+      <div>
+        <tl-writeokr v-if="createokrDrawer" :userName="userName"></tl-writeokr>
+      </div>
+    </el-drawer>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex';
+import writeOkr from './component/writeOkr/index';
 import Server from './server';
 
 const server = new Server();
@@ -22,22 +36,35 @@ const server = new Server();
 export default {
   name: 'okr',
   components: {
+    'tl-writeokr': writeOkr,
   },
   data() {
     return {
       server,
-      activeName: 'first',
+      activeName: 'myOkr',
     };
   },
   computed: {
+    ...mapState('common', {
+      createokrDrawer: (state) => state.createokrDrawer,
+      userInfo: (state) => state.userInfo,
+    }),
   },
   created() {
+    this.departmentName = this.userInfo.orgParentName || '部门';
   },
   methods: {
+    ...mapMutations('common', ['setCreateokrDrawer']),
     goRoutesss(tab, event) {
       console.log(tab);
       console.log(event);
-      this.go(tab);
+      this.go(this.activeName);
+    },
+    goWriteOkr() {
+      this.setCreateokrDrawer(true);
+    },
+    handleClose() {
+      this.setCreateokrDrawer(false);
     },
   },
   watch: {
