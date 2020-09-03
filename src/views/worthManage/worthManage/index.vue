f040fa0b774eea792ef0d8843cfcaf23544fdc9e
chenxiang166 <chenxiang166@crc.com.hk>
2020-09-03 16:14:44 +0800

    价值观

--- src/views/worthManage/worthManage/index.vue
+++ src/views/worthManage/worthManage/index.vue
@@ -4,16 +4,38 @@
       <div class="page-title">价值观事件簿</div>
       <div class="operating-panel">
         <el-form ref="ruleForm" :inline="true" class="tl-form-inline">
-          <el-form-item>
-            <el-input
-              maxlength="64"
-              @keyup.enter.native="searchList"
-              v-model.trim="keyWord"
-              placeholder="请输入您要查找的关键字"
-              class="tl-input-search"
-            >
-              <i class="el-icon-search" slot="prefix" @click="searchList"></i>
-            </el-input>
+          <el-form-item label="组织">
+            <div @click="showCascader=!showCascader">
+              <el-input v-model="formData.orgName" style="width: 200px;"></el-input>
+            </div>
+            <el-cascader-panel
+              v-model="formData.orgFullId"
+              :style="{display: showCascader ? '' : 'none'}"
+              :options="departmentData"
+              :show-all-levels="false"
+              @change="selectIdChange"
+              :props="{ checkStrictly: true, expandTrigger: 'hover',value:'orgFullId',label:'orgName',children:'children' }"
+            ></el-cascader-panel>
+          </el-form-item>
+          <el-form-item label="价值观">
+            <el-select v-model.trim="formData.worthId" placeholder="请选择" @change="changeSearch">
+              <el-option
+                v-for="item in worthList"
+                :key="item.id"
+                :label="item.cultureName"
+                :value="item.id"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="评分">
+            <el-select v-model.trim="formData.scoreId" placeholder="请选择" @change="changeSearch">
+              <el-option
+                v-for="item in CONST.SCORE_LIST"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </el-form-item>
         </el-form>
       </div>
@@ -27,13 +49,29 @@
       >
         <div slot="tableContainer" class="table-container">
           <el-table :data="tableData" class="tl-table">
-            <el-table-column prop="userName" label="员工" min-width="140"></el-table-column>
-            <el-table-column prop="updateTime" label="时间" min-width="160"></el-table-column>
+            <el-table-column prop="userName" label="员工" min-width="140">
+              <template slot-scope="scope">
+                <div>
+                  <div class="user-info">
+                    <img v-if="scope.row.headUrl" :src="scope.row.headUrl" alt />
+                    <div
+                      v-else-if="scope.row.userName"
+                      class="user-name"
+                    >{{scope.row.userName.substring(scope.row.userName.length-2)}}</div>
+                  </div>
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column prop="cultureName" label="支撑价值观" min-width="180"></el-table-column>
             <el-table-column prop="workContent" label="工作项" min-width="120"></el-table-column>
-            <el-table-column prop="cultureName" label="支撑的价值观" min-width="180"></el-table-column>
+            <el-table-column prop="updateTime" label="评分" min-width="160"></el-table-column>
+            <el-table-column prop="updateTime" label="评分时间" min-width="160"></el-table-column>
+            <el-table-column prop="updateTime" label="支撑时间" min-width="160"></el-table-column>
             <el-table-column fixed="right" label="操作" width="180">
               <template slot-scope="scope">
                 <el-button @click="goWeekly(scope.row)" type="text" class="tl-btn">查看周报</el-button>
+                <el-button @click="goWeekly(scope.row)" type="text" class="tl-btn">评分</el-button>
+                <el-button @click="goWeekly(scope.row)" type="text" class="tl-btn">评分详情</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -45,7 +83,8 @@
 
 <script>
 import crcloudTable from '@/components/crcloudTable';
-import Server from './server';
+import Server from '../server';
+import CONST from '../const';
 
 const server = new Server();
 export default {
@@ -53,32 +92,90 @@ export default {
   data() {
     return {
       server,
+      CONST,
       total: 0,
       currentPage: 1,
       pageSize: 10,
-      keyWord: '',
+      formData: {
+        scoreId: '',
+        worthId: '',
+        orgName: '',
+        orgFullId: '',
+      },
       tableData: [],
+      worthList: [],
+      departmentData: [],
+      orgFullIdList: [],
+      showCascader: false,
     };
   },
   components: {
     'tl-crcloud-table': crcloudTable,
   },
-  created() {
-    this.searchList();
-  },
   mixins: [global],
+  mounted() {
+    this.server.queryCultureList().then((res) => {
+      if (res.code == '200') {
+        this.worthList = res.data;
+        if (this.worthList.length > 0) {
+          this.worthList.push({
+            id: '',
+            cultureName: '全部',
+          });
+        }
+      }
+    });
+    this.getOrgTable();
+  },
   methods: {
     searchList() {
-      this.server.weeklyEvents({
-        currentPage: this.currentPage,
-        keyWord: this.keyWord,
-        pageSize: this.pageSize,
-      }).then((res) => {
+      if (this.formData.orgFullId) {
+        this.server.weeklyEvents({
+          cultureId: this.formData.worthId,
+          score: this.formData.scoreId,
+          orgFullId: this.formData.orgFullId,
+          currentPage: this.currentPage,
+          pageSize: this.pageSize,
+        }).then((res) => {
+          if (res.code == '200') {
+            this.tableData = res.data.content;
+            this.total = res.data.total;
+          }
+        });
+      }
+    },
+    selectIdChange(data) {
+      this.showCascader = false;
+      this.orgFullId = data[data.length - 1];
+      this.orgFullIdList = this.orgFullId.split(':');
+      this.orgFullIdList.splice(this.orgFullIdList.length - 1, 1);
+      this.getOrgName(this.departmentData, 0);
+    },
+    getOrgTable() {
+      // 查询组织树
+      this.server.getOrgTable().then((res) => {
         if (res.code == '200') {
-          this.tableData = res.data.content;
-          this.total = res.data.total;
+          if (res.data) {
+            this.departmentData.push(res.data);
+            this.formData.orgFullId = this.departmentData[0].orgFullId;
+            this.searchList();
+          }
         }
       });
+    },
+    getOrgName(data, index) {
+      data.forEach((item) => {
+        if (this.orgFullIdList[index] == item.orgId) {
+          if (item.children && item.children.length > 0 && this.orgFullIdList[index + 1]) {
+            this.getOrgName(item.children, index + 1);
+          } else if ((index + 1) == this.orgFullIdList.length) {
+            this.formData.orgName = item.orgName;
+          }
+        }
+      });
+    },
+    changeSearch() {
+      this.searchList();
     },
     goWeekly(data) {
       console.log(data);
--- src/views/worthManage/worthManage/server.js
+++
@@ -1,7 +0,0 @@
-import ServerBase from '@/ajax/serverBase';
-
-export default class Server extends ServerBase {
-  weeklyEvents(param) {
-    return this._ajaxPost('gateway/weekly-service/weekly/events', param);
-  }
-}
