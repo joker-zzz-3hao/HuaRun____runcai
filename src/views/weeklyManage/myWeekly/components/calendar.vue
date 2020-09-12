9dcb8ad072dc46b6881abbfb973f7d472fd7369b
wangzhiren5 <wangzhiren5@crc.com.hk>
2020-09-12 17:52:30 +0800

    回到本周功能优化

--- src/views/weeklyManage/myWeekly/components/calendar.vue
+++ src/views/weeklyManage/myWeekly/components/calendar.vue
@@ -273,7 +273,7 @@ export default {
       }
     },
     goCurrentWeek() {
-      window.location.reload();
+      this.getWeek(this.dateFormat('YYYY-mm-dd', new Date()));
     },
   },
   watch: {},
