d158bc8d52c60e73c032f3cbd1cafe7dcc22d94c
Lin Xinquan 林心荃 <xinquan.lin@163.com>
2020-09-16 18:55:19 +0800

    部门的类型

--- src/views/okrManage/myOkr/departmentOkr.vue
+++ src/views/okrManage/myOkr/departmentOkr.vue
@@ -18,7 +18,7 @@
             <dt>
               <em>OKR类型</em>
             </dt>
-            <dd>{{CONST.OKR_TYPE_MAP[okrMain.okrBelongType]}}</dd>
+            <dd>{{CONST.OKR_TYPE_MAP[okrMain.okrBelongType || 1]}}</dd>
           </dl>
           <dl class="okr-responsible">
             <dt>
