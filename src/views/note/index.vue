0c9fa9a63787f04d628e09e6da56f372e48103ad
chenxiang166 <chenxiang166@crc.com.hk>
2020-09-16 13:56:12 +0800

    笔记

---
+++ src/views/note/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'note',
+  components: {},
+  props: {},
+  data() {
+    return {
+    };
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+  updated() {},
+  beforeDestroy() {},
+};
+</script>
