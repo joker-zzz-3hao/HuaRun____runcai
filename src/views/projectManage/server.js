3254433bddc1205d19251c2f1a2a19045560ab87
chenxiang166 <chenxiang166@crc.com.hk>
2020-10-10 09:19:24 +0800

    项目

--- src/views/projectManage/server.js
+++ src/views/projectManage/server.js
@@ -1,6 +1,14 @@
 import ServerBase from '@/ajax/serverBase';
 
 export default class Server extends ServerBase {
+  projectPageList(param) {
+    return this._ajaxPost('gateway/project-service/project/query/projectPageList', param);
+  }
+
+  createProject(param) {
+    return this._ajaxPost('gateway/project-service/project/create/project', param);
+  }
+
   getOrgTable(param) {
     return this._ajaxPost('gateway/talent-query/okr/query/orgTable', param);
   }
