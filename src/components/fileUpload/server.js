406311b70388028e01013254ec16d729e0cd943e
Lin Xinquan 林心荃 <xinquan.lin@163.com>
2020-10-10 13:52:36 +0800

    上传文件

---
+++ src/components/fileUpload/server.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: 王益
+ * @Date: 2019-01-21 19:29:55
+ * @LastEditors: 王益
+ * @LastEditTime: 2019-04-15 16:18:05
+ * @Description: file content
+ */
+
+import ServerBase from '@/ajax/serverBase';
+
+export default class Server extends ServerBase {
+  deleteFile(param = {}) {
+    return this._ajaxPost(`gateway/crcloud-cert/resource/delete?resourceId=${param.resourceId}`);
+  }
+}
