5ae9812178b6602809f4dc490341556af6bb0b8c
chenxiang166 <chenxiang166@crc.com.hk>
2020-10-10 10:22:52 +0800

    设置默认路由

---
+++ src/views/teamsManage/const.js
@@ -0,0 +1,11 @@
+const OVERVIEW_LIST = [
+  { value: 'overview', label: '主页' },
+  { value: 'myOkr', label: 'OKR-我的OKR' },
+  { value: 'okrMaps', label: 'OKR-OKR地图' },
+  { value: 'myWeekly', label: '周报' },
+  { value: 'myTask', label: '任务' },
+];
+
+export default {
+  OVERVIEW_LIST,
+};
