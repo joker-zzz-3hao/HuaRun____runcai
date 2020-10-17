9e5c17c2615f886c73af3c82409e472860f9a958
tengwei6 <tengwei6@crc.com.hk>
2020-10-17 17:02:46 +0800

    hebing

--- src/views/teamsManage/component/teamCard.vue
+++ src/views/teamsManage/component/teamCard.vue
@@ -12,12 +12,12 @@
             </div>
           </div>
           <em>{{ node.node.orgLeader }}</em>
-          <span>(部门负责人)</span>
+          <span>(组织负责人)</span>
         </template>
-        <span v-else>提示：此部门尚未设置部门负责人</span>
+        <span v-else>提示：此组织尚未设置负责人</span>
       </dt>
       <dd class="user-name-txt">
-        <span>{{ node.node.orgType == "0" ? "实" : "虚" }}</span>
+        <span>{{ node.node.orgType == "0" ? "实体组织" : "虚拟组织" }}</span>
         <em>{{ node.node.orgName }}</em>
       </dd>
       <dd v-if="node.node.orgType == '1'" class="is-operational">
@@ -33,7 +33,7 @@
       <div class="icon-bg-big">
         <i class="el-icon-plus"></i>
       </div>
-      <span>创建虚拟团队</span>
+      <span>创建虚拟组织</span>
     </div>
   </div>
 </template>
