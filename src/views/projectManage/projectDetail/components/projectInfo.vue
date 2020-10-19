fb3d25b1ebee98b30e42f25851a8829dc7613cf9
chenxiang166 <chenxiang166@crc.com.hk>
2020-10-19 20:36:29 +0800

    展开

--- src/views/projectManage/projectDetail/components/projectInfo.vue
+++ src/views/projectManage/projectDetail/components/projectInfo.vue
@@ -11,6 +11,12 @@
         }}</span>
       </div>
       <div>
+        <div :class="openFlag ? 'open' : 'false'">
+          项目描述：{{ `${baseInfo.projectDescription || "--"}` }}
+        </div>
+        <div @click="openFlag = !openFlag">展开</div>
+      </div>
+      <div>
         <span>项目经理：{{ `${baseInfo.projectManager || "--"}` }}</span>
         <span
           >项目所属部门：{{ `${baseInfo.projectApplyDepName || "--"}` }}</span
@@ -129,6 +135,7 @@
       ref="addMember"
       v-if="showAddMember"
       :server="server"
+      :codes="codes"
       @addSuccess="addSuccess"
     ></tl-add-member>
   </div>
@@ -152,6 +159,8 @@ export default {
       showAddMember: false,
       tableData: [],
       isManage: false,
+      openFlag: false,
+      codes: [],
     };
   },
   components: {
@@ -187,6 +196,13 @@ export default {
         }
       });
     }
+    this.server.queryByCodes({
+      codes: ['PROJECT_TECH_TYPE', 'PROJECT_EMPLOYEE_LEVEL', 'PROJECT_EMPLOYEE_COMPANY'],
+    }).then((res) => {
+      if (res.code == '200') {
+        this.codes = res.data;
+      }
+    });
   },
   methods: {
     deleteMember(data) {
