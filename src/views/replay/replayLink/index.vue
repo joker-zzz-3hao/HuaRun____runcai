87ff6aa598098e1580684be787c510b47097a79c
xuzhipeng <357089360@qq.com>
2020-11-03 16:53:05 +0800

    修改操作历史

--- src/views/replay/replayLink/index.vue
+++ src/views/replay/replayLink/index.vue
@@ -1,61 +1,6 @@
 <template>
   <div class="replay-link">
-    <dl class="replay-info">
-      <dt>
-        <div class="replay-title">
-          <span>复盘对象:</span><em>{{ okrMain.okrMainVo.periodName }}</em
-          ><em
-            >({{
-              dateFormat("YYYY/mm/dd", new Date(okrMain.okrMainVo.startTime)) +
-              "~" +
-              dateFormat("YYYY/mm/dd", new Date(okrMain.okrMainVo.endTime))
-            }})</em
-          >
-        </div>
-        <el-button plain @click="$router.back()" class="tl-btn amt-border-slip">
-          返回
-          <span class="lines"></span>
-        </el-button>
-      </dt>
-      <dd class="dl-list-group">
-        <dl class="dl-item user-info">
-          <dd v-if="okrMain.okrMainVo.headUrl">
-            <img :src="okrMain.okrMainVo.headUrl" alt />
-          </dd>
-          <dd v-else class="user-name">
-            <em>{{ cutName(okrMain.okrMainVo.userName) }}</em>
-          </dd>
-          <dd>{{ okrMain.okrMainVo.userName }}</dd>
-        </dl>
-        <dl class="dl-item">
-          <dt><span>复盘时间</span></dt>
-          <dd>
-            <em>{{
-              okrMain.okrMainVo.reviewCommitTime
-                ? okrMain.okrMainVo.reviewCommitTime
-                : "--"
-            }}</em>
-          </dd>
-        </dl>
-        <dl class="dl-item">
-          <dt><span>OKR进度</span></dt>
-          <dd>
-            <el-progress
-              type="circle"
-              :percentage="parseInt(okrMain.okrMainVo.okrProgress, 10) || 0"
-              :width="70"
-              :stroke-width="5"
-              color="#4ccd79"
-              class="tl-progress-circle"
-            ></el-progress>
-          </dd>
-        </dl>
-      </dd>
-      <dd class="flex-end">
-        <em v-if="okrMain.okrMainVo.reviewType == 1">以关键结果KR复盘</em>
-        <em v-if="okrMain.okrMainVo.reviewType == 0">以目标O复盘</em>
-      </dd>
-    </dl>
+    <tl-replayUser :okrMain="okrMain"></tl-replayUser>
     <tl-kr-detail
       v-if="okrMain.okrMainVo.reviewType == 1"
       @getView="getOkrReviewDetail"
@@ -66,26 +11,7 @@
       @getView="getOkrReviewDetail"
       :okrMain="okrMain"
     />
-    <dl class="dl-card-panel replay-record">
-      <dt><em>复盘记录</em></dt>
-      <dd>
-        <div class="tl-custom-timeline">
-          <dl class="timeline-list">
-            <dd v-for="(item, index) in activities" :key="index">
-              <div class="list-info">
-                <div class="list-title">{{ item.createTime }}</div>
-                <div class="list-cont">
-                  <div class="operate-type">
-                    <em>{{ item.userName }}</em>
-                    <span>{{ item.content }}</span>
-                  </div>
-                </div>
-              </div>
-            </dd>
-          </dl>
-        </div>
-      </dd>
-    </dl>
+    <tl-replayHistory :activities="activities"></tl-replayHistory>
   </div>
 </template>
 
