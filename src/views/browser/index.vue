2407d77a43d60ab55873ce962a199427050ea4bb
wangzhiren5 <wangzhiren5@crc.com.hk>
2020-11-12 20:21:25 +0800

    logo样式调整

--- src/views/browser/index.vue
+++ src/views/browser/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="browser-background-color"></div>
+  <div class="browser-background-box">
+    <div class="logo-browser"></div>
+    <p>华润润才平台暂时不支持您所用的浏览器</p>
+    <p>
+      为了您更好的使用润才，推荐使用
+      <span>Google Chrome浏览器</span
+      >、<span>Firefox浏览器</span>，以获得最佳产品体验。
+    </p>
+  </div>
 </template>
 
 <script>
@@ -19,7 +27,26 @@ export default {
 };
 </script>
 <style lang="css">
-.browser-background-color {
-  background: url("~@/assets/images/browaer.png") no-repeat;
+.browser-background-box {
+  text-align: center;
+  width: 100%;
+  margin-top: 10%;
+}
+.logo-browser {
+  background: url(~@/assets/images/logo-browser.png) no-repeat center;
+  width: 368px;
+  height: 196px;
+  background-size: cover;
+  display: block;
+  margin: 0 auto;
+}
+.browser-background-box div,
+.browser-background-box p {
+  font-size: 28px;
+  margin-top: 20px;
+  line-height: 28px;
+}
+.browser-background-box p span {
+  color: #6b60f1;
 }
 </style>
