3f07b5f05f8bbf3f01cc0bf8de4a6c6844bee634
Lin Xinquan 林心荃 <xinquan.lin@163.com>
2020-11-17 17:44:21 +0800

    进度条

--- src/components/process.vue
+++ src/components/process.vue
@@ -2,9 +2,9 @@
   <div class="tl-progress-bar">
     <div
       class="progress-bg"
+      :style="{ width: `${width}px`, marginLeft: `${marginLeft}px` }"
       v-for="item in processList"
       :key="item.id"
-      ref="progressBg"
     >
       <div class="current-progress" :style="item.width"></div>
     </div>
@@ -26,14 +26,14 @@ export default {
       type: Number,
       default: 0,
     },
-    // width: {
-    //   type: Number,
-    //   default: 16,
-    // },
-    // marginLeft: {
-    //   type: Number,
-    //   default: 2,
-    // },
+    width: {
+      type: Number,
+      default: 16,
+    },
+    marginLeft: {
+      type: Number,
+      default: 2,
+    },
     showNumber: {
       type: Boolean,
       default: true,
@@ -42,27 +42,20 @@ export default {
   mounted() {
     this.changeWidth();
   },
-  computed: {
-    // progressWidth() {
-    //   console.log(this.$refs.progressBg);
-    //   return this.$refs.progressBg[0].offsetWidth;
-    // },
-  },
   methods: {
     changeWidth() {
       const self = this;
       self.processList = [];
       const a = parseInt(self.data / 20, 10);
       const b = self.data % 20;
-      const progressWidth = this.refs.progressBg.clientWidth;
       for (let i = 0; i < 5; i += 1) {
         if (i < a) {
           self.processList.push({
-            width: `width: ${progressWidth}px;`,
+            width: `width: ${this.width}px;`,
           });
         } else if (i == a) {
           self.processList.push({
-            width: `width: ${(b / 20) * progressWidth}px`,
+            width: `width: ${(b / 20) * this.width}px`,
           });
         } else {
           self.processList.push({
@@ -70,7 +63,6 @@ export default {
           });
         }
       }
-      console.log(self.processList);
     },
   },
   watch: {
