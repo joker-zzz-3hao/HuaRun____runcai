55c9770a1e59448aeae331056ab8e369ac4b7673
tengwei6 <tengwei6@crc.com.hk>
2020-11-18 10:10:01 +0800

    gx

--- src/components/process.vue
+++ src/components/process.vue
@@ -2,11 +2,15 @@
   <div class="tl-progress-bar">
     <div
       class="progress-bg"
-      :style="{ width: `${width}px`, marginLeft: `${marginLeft}px` }"
-      v-for="item in processList"
-      :key="item.id"
+      v-for="(item, index) in testList"
+      :key="item"
+      ref="progressBg"
     >
-      <div class="current-progress" :style="item.width"></div>
+      <div
+        v-if="processList.length > 0"
+        class="current-progress"
+        :style="processList[index].width"
+      ></div>
     </div>
     <div v-if="showNumber" class="progress-number">{{ data }}%</div>
   </div>
@@ -18,6 +22,7 @@ export default {
   data() {
     return {
       processList: [],
+      testList: [1, 2, 3, 4, 5],
     };
   },
   components: {},
@@ -26,21 +31,18 @@ export default {
       type: Number,
       default: 0,
     },
-    width: {
-      type: Number,
-      default: 16,
-    },
-    marginLeft: {
-      type: Number,
-      default: 2,
-    },
     showNumber: {
       type: Boolean,
       default: true,
     },
   },
   mounted() {
-    this.changeWidth();
+    this.$nextTick(() => {
+      this.test = this.$refs.progressBg;
+      console.log(`testHeight:${this.test[0].clientHeight}`);
+      console.log(`testWidth:${this.test[0].clientWidth}`);
+      this.changeWidth();
+    });
   },
   methods: {
     changeWidth() {
@@ -48,14 +50,15 @@ export default {
       self.processList = [];
       const a = parseInt(self.data / 20, 10);
       const b = self.data % 20;
+      const progressWidth = this.test[0].clientWidth;
       for (let i = 0; i < 5; i += 1) {
         if (i < a) {
           self.processList.push({
-            width: `width: ${this.width}px;`,
+            width: `width: ${progressWidth}px;`,
           });
         } else if (i == a) {
           self.processList.push({
-            width: `width: ${(b / 20) * this.width}px`,
+            width: `width: ${(b / 20) * progressWidth}px`,
           });
         } else {
           self.processList.push({
