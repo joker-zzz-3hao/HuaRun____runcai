debda1700f3d70203151dba9007316eafd0a786b
Lin Xinquan 林心荃 <xinquan.lin@163.com>
2020-11-24 14:43:16 +0800

    上传头像

--- src/views/personalCenter/server.js
+++ src/views/personalCenter/server.js
@@ -9,4 +9,9 @@ export default class Server extends ServerBase {
   queryByTenantIdAndUserId(param) {
     return this._ajaxGet('gateway/privilege-service/privilege/function/queryByTenantIdAndUserId', param);
   }
+
+  // 更新文件状态
+  updateResource(param) {
+    return this._ajaxPost(`gateway/system-service/sys/attachment/updateResource?resourceId=${param.resourceId}&sourceType=${param.sourceType}`, param);
+  }
 }
