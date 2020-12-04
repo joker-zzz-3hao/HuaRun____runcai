fd3078876d77193fa9fec8e956db93ba879bd929
xuzhipeng <357089360@qq.com>
2020-12-04 18:42:33 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -49,9 +49,9 @@ function judeBrowser() {
   const isEdge = userAgent.indexOf('Edge') > -1; // 判断是否IE的Edge浏览器
   const isFF = userAgent.indexOf('Firefox') > -1; // 判断是否Firefox浏览器
   const isSafari = userAgent.indexOf('Safari') > -1
-            && userAgent.indexOf('Chrome') == -1; // 判断是否Safari浏览器
+    && userAgent.indexOf('Chrome') == -1; // 判断是否Safari浏览器
   const isChrome = userAgent.indexOf('Chrome') > -1
-            && userAgent.indexOf('Safari') > -1; // 判断Chrome浏览器
+    && userAgent.indexOf('Safari') > -1; // 判断Chrome浏览器
 
   if (isEdge) {
     return 'Edge';
