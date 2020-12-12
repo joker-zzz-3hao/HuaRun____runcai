89774b8f6d15283a790e9cba17659847ca9516c6
tengwei6 <tengwei6@crc.com.hk>
2020-12-12 14:40:05 +0800

    更新

--- src/views/projectManage/mainHours/components/descModel.vue
+++ src/views/projectManage/mainHours/components/descModel.vue
@@ -4,17 +4,51 @@
     :visible="visible"
     @close="close"
     :before-close="close"
+    :title="drawerTitle"
     :close-on-click-modal="false"
-    custom-class="approval"
+    custom-class="approval-list-detail"
     class="tl-dialog"
-    width="620px"
+    width="820px"
   >
-  <div>工作项:{{info.workContent}} </div>
+    <div class="dl-group">
+      <dl>
+        <dt><span>工作项</span></dt>
+        <dd>
+          <em>{{ info.workContent }}</em>
+        </dd>
+      </dl>
+      <dl>
+        <dt><span>工作内容</span></dt>
+        <dd>
+          <em>{{ info.workDesc }}</em>
+        </dd>
+      </dl>
+      <dl>
+        <dt><span>工作进度</span></dt>
+        <dd>
+          <em>{{ info.workProgress + "%" }}</em>
+        </dd>
+      </dl>
+      <dl>
+        <dt><span>填报工时</span></dt>
+        <dd>
+          <em>{{ day + "天" }}({{ textDay }})</em>
+        </dd>
+      </dl>
+      <dl>
+        <dt><span>工时日期</span></dt>
+        <dd>
+          <em>{{ week }}</em>
+        </dd>
+      </dl>
+    </div>
 
-    <div>工作内容:{{info.workDesc}}</div>
-      <div>进度:{{info.workProgress + '%'}}</div>
-        <div>填报工时:{{day+'天'}}({{textDay}})</div>
-        <div>工时日期:{{week}}</div>
+    <!-- <div>工作项:</div>
+
+    <div>工作内容:</div>
+    <div>工作进度:</div>
+    <div>填报工时:</div>
+    <div>工时日期:</div> -->
   </el-dialog>
 </template>
 
@@ -38,6 +72,7 @@ export default {
       textDay: '',
       day: '',
       week: '',
+      drawerTitle: '工时详情',
     };
   },
   // components: {
