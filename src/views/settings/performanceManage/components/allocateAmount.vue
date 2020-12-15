bbb71eee3cdc89c5fd57468cbd786cf9114582d5
wangzhiren5 <wangzhiren5@crc.com.hk>
2020-12-15 13:52:32 +0800

    绩效管理bug修复

--- src/views/settings/performanceManage/components/allocateAmount.vue
+++ src/views/settings/performanceManage/components/allocateAmount.vue
@@ -49,7 +49,6 @@
             <span>{{ item.value + item.unit }}</span
             ><el-input
               v-model="item.applyValue"
-              @input="inputChange(item)"
               @blur="inputChange(item)"
             ></el-input
             ><span>个</span>
@@ -187,6 +186,10 @@ export default {
       this.amountDataList.forEach((element) => {
         if (element.periodRuleId == periodRuleId) {
           this.amountData = { ...element };
+          // 将数值复制一份;
+          this.amountData.periodRuleDetailList.forEach((data) => {
+            data.applyValueCopy = data.applyValue;
+          });
           this.amountData.periodRuleId = '';
           // 如果是新增则提交的periodRuleId为空；否则赋值
           this.detailList.forEach((detail) => {
@@ -212,9 +215,24 @@ export default {
               if (!(/(^[0-9]\d*$)/.test(Number(amountItem.applyValue)))) {
                 element1.showError = true;
                 element1.errorText = '请填写正整数';
-              } else if (amountItem.applyValue > element2.applyValue) { // 2、分配的数值大小，不能大于剩余可用数量
-                element1.showError = true;
-                element1.errorText = '数量不能大于剩余数量';
+              } else if (!element1.periodRuleId) {
+                // 新增：分配的数值大小，不能大于剩余可用数量；
+                if (amountItem.applyValue > element2.applyValue) {
+                  element1.showError = true;
+                  element1.errorText = '数量不能大于剩余数量';
+                } else {
+                  element1.showError = false;
+                  element1.errorText = '';
+                }
+              } else if (element1.periodRuleId) {
+                // 编辑：新增的数据不能大于剩余的数量
+                if (amountItem.applyValue - amountItem.applyValueCopy > element2.applyValue) {
+                  element1.showError = true;
+                  element1.errorText = '数量不能大于剩余数量';
+                } else {
+                  element1.showError = false;
+                  element1.errorText = '';
+                }
               } else {
                 element1.showError = false;
                 element1.errorText = '';
@@ -236,25 +254,58 @@ export default {
       this.amountData.applyType = 2;// 类型：部门
       this.amountData.applyId = this.rowData.orgId;// 部门id
       this.amountData.sourcePeriodRuleId = this.periodRuleId;
-      this.amountData.periodRuleDetailList.forEach((data) => {
-        data.periodRuleDetailId = '';
-        data.periodDetailId = '';
-      });
+      // this.amountData.periodRuleDetailList.forEach((data) => {
+      // data.periodRuleDetailId = '';
+      // data.periodDetailId = '';
+      // });
       // 校验表单
-      for (let i = 0; i < this.amountData.periodRuleDetailList.length; i += 1) {
-        const element = this.amountData.periodRuleDetailList[i];
-        if (element.showError) {
-          this.$message.error('填写数据错误，请修改后重试。');
-          return;
-        }
-      }
-      this.server.addOrUpdateAmount([this.amountData]).then((res) => {
-        if (res.code == 200) {
-          this.$message.success('成功');
-          this.$emit('refreshPage');
-          this.close();
-        }
+      let validateStatus = true;
+      this.amountData.periodRuleDetailList.forEach((element1) => {
+        this.remainAmount.periodRuleDetailList.forEach((element2) => {
+          if (element1.periodRuleDetailId == element2.periodRuleDetailId) {
+            // 1、正整数数字
+            if (!(/(^[0-9]\d*$)/.test(Number(element1.applyValue)))) {
+              element1.showError = true;
+              element1.errorText = '请填写正整数';
+              validateStatus = false;
+            } else if (!element1.periodRuleId) {
+              // 新增：分配的数值大小，不能大于剩余可用数量；
+              if (element1.applyValue > element2.applyValue) {
+                element1.showError = true;
+                element1.errorText = '数量不能大于剩余数量';
+                validateStatus = false;
+              } else {
+                element1.showError = false;
+                element1.errorText = '';
+              }
+            } else if (element1.periodRuleId) {
+              // 编辑：新增的数据不能大于剩余的数量
+              if (element1.applyValue - element1.applyValueCopy > element2.applyValue) {
+                element1.showError = true;
+                element1.errorText = '数量不能大于剩余数量';
+                validateStatus = false;
+              } else {
+                element1.showError = false;
+                element1.errorText = '';
+              }
+            } else {
+              element1.showError = false;
+              element1.errorText = '';
+            }
+          }
+        });
       });
+      if (validateStatus) {
+        this.server.addOrUpdateAmount([this.amountData]).then((res) => {
+          if (res.code == 200) {
+            this.$message.success('成功');
+            this.$emit('refreshPage');
+            this.close();
+          }
+        });
+      } else {
+        this.$message.error('填写数据错误，请修改后重试。');
+      }
     },
   },
   watch: {},
