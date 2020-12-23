0b4358fbb1fb239c41ee870bac199852f18da864
tengwei6 <tengwei6@crc.com.hk>
2020-12-23 19:53:46 +0800

    gx

--- src/views/projectManage/hoursCollection/components/queryHistory.vue
+++ src/views/projectManage/hoursCollection/components/queryHistory.vue
@@ -1,59 +1,83 @@
 <template>
-   <tl-crcloud-table
+  <div class="working-hours-collection detaile">
+    <div class="operating-area">
+      <div class="operating-box">
+        <el-button plain @click="$router.back()" class="tl-btn amt-border-slip">
+          返回
+          <span class="lines"></span>
+        </el-button>
+      </div>
+    </div>
+    <div class="cont-area">
+      <tl-crcloud-table
         :total="total"
         :currentPage.sync="currentPage"
         :pageSize.sync="pageSize"
         @searchList="searchList"
       >
         <div slot="tableContainer" class="table-container project-members">
-    <el-table :data="tableData"  class="tl-table">
-      <el-table-column type="expand">
-        <template slot-scope="props">
-          <el-table :data="props.row.projectSupplementList" row-key="supplementId">
-
-            <el-table-column prop="supplementUser" label="姓名"></el-table-column>
-            <el-table-column prop="userLevel" label="级别">
-                <template slot-scope="scope">
-              {{getName(scope.row.userLevel,levelList)}}
-                </template>
+          <el-table :data="tableData" class="tl-table">
+            <el-table-column type="expand">
+              <template slot-scope="props">
+                <el-table
+                  :data="props.row.projectSupplementList"
+                  row-key="supplementId"
+                >
+                  <el-table-column
+                    prop="supplementUser"
+                    label="姓名"
+                  ></el-table-column>
+                  <el-table-column prop="userLevel" label="级别">
+                    <template slot-scope="scope">
+                      {{ getName(scope.row.userLevel, levelList) }}
+                    </template>
+                  </el-table-column>
+                  <el-table-column prop="userPost" label="职能">
+                    <template slot-scope="scope">
+                      {{ getName(scope.row.userPost, funcList) }}
+                    </template>
+                  </el-table-column>
+                  <el-table-column prop="userCompany" label="所属公司">
+                    <template slot-scope="scope">
+                      {{ getName(scope.row.userCompany, companyList) }}
+                    </template>
+                  </el-table-column>
+                  <el-table-column prop="finalScore" label="工时时间范围">
+                    <template slot-scope="scope">
+                      {{
+                        dateFormat("YYYY-mm-dd", scope.row.supplementTimeBegin)
+                      }}至
+                      {{
+                        dateFormat("YYYY-mm-dd", scope.row.supplementTimeEnd)
+                      }}
+                    </template>
+                  </el-table-column>
+                  <el-table-column prop="supplementTime" label="补录工时(天)">
+                  </el-table-column>
+                  <el-table-column prop="supplementContent" label="工时内容">
+                  </el-table-column>
+                </el-table>
+              </template>
             </el-table-column>
-            <el-table-column prop="userPost" label="职能">
-               <template slot-scope="scope">
-              {{getName(scope.row.userPost,funcList)}}
-               </template>
+            <el-table-column label="补录时间" prop="supplementSubmitTimeDate">
+              <template slot-scope="scope">
+                {{
+                  dateFormat("YYYY-mm-dd", scope.row.supplementSubmitTimeDate)
+                }}
+              </template>
             </el-table-column>
-            <el-table-column prop="userCompany" label="所属公司">
-                <template slot-scope="scope">
-              {{getName(scope.row.userCompany,companyList)}}
-               </template>
-            </el-table-column>
-             <el-table-column prop="finalScore" label="工时时间范围">
-                <template slot-scope="scope">
-                  {{dateFormat('YYYY-mm-dd',scope.row.supplementTimeBegin)}}至
-                  {{dateFormat('YYYY-mm-dd',scope.row.supplementTimeEnd)}}
-                  </template>
-            </el-table-column>
-              <el-table-column prop="supplementTime" label="补录工时(天)">
-            </el-table-column>
-                 <el-table-column prop="supplementContent" label="工时内容">
+            <el-table-column
+              label="补录人"
+              prop="supplementSubmitter"
+            ></el-table-column>
+            <el-table-column prop="timeSum" label="工时(天)"> </el-table-column>
+            <el-table-column prop="laborCost" label="人力成本(人民币)">
             </el-table-column>
           </el-table>
-        </template>
-      </el-table-column>
-      <el-table-column  label="补录时间" prop="supplementSubmitTimeDate">
-         <template slot-scope="scope">
-        {{dateFormat('YYYY-mm-dd',scope.row.supplementSubmitTimeDate)}}
-         </template>
-      </el-table-column>
-      <el-table-column  label="补录人" prop="supplementSubmitter"></el-table-column>
-      <el-table-column prop="timeSum" label="工时(天)">
-      </el-table-column>
-      <el-table-column prop="laborCost" label="人力成本(人民币)">
-      </el-table-column>
-
-    </el-table>
         </div>
-   </tl-crcloud-table>
+      </tl-crcloud-table>
+    </div>
+  </div>
 </template>
 
 <script>
