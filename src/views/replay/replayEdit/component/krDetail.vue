9548361f6b218c2b361d509e24a10e1ea2b9448b
tengwei6 <tengwei6@crc.com.hk>
2020-12-29 17:26:33 +0800

    更新

--- src/views/replay/replayEdit/component/krDetail.vue
+++ src/views/replay/replayEdit/component/krDetail.vue
@@ -141,6 +141,7 @@
               .pptx,
               .xlsx"
                     tips="支持jpg、jpeg、png、doc、docx、xslx、pptx，最多上传10个文件，单个文件不超过10M"
+                    class="upload-wrap"
                   ></file-upload>
                 </dd>
               </dl>
@@ -199,8 +200,12 @@
                   </template>
                 </dl>
               </template>
-              <div @click="openMore(list)">
-                <i :class="list.openAdvantage === true ? 'close' : 'open'"></i>
+              <div @click="openMore(list)" class="fold-extend">
+                <i
+                  :class="
+                    list.openAdvantage === true ? 'has-fold' : 'has-extend'
+                  "
+                ></i>
                 <span v-if="list.openAdvantage">收起</span>
                 <span v-else>展开</span>
               </div>
